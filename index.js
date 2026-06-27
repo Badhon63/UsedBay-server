aa4ca6b9576144ed13b0963c9e3efb7479abdf20
Badhon63 <jianrobiul7898@gmail.com>
2026-06-27 13:22:14 +0600

    added product creation endpoint

---
+++ index.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const express = require("express");
+const PORT = process.env.PORT;
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send({ message: "homepage" });
+});
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const client = new MongoClient(process.env.MONGO_DB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("usedbay");
+    const productsCollection = db.collection("products");
+
+    app.post("/api/products", async (req, res) => {
+      const data = req.body;
+      const result = await productsCollection.insertOne(data);
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(PORT, () => {
+  console.log("server started.");
+});
